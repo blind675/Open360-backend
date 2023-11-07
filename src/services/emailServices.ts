d638c15223330361576f522a3eec85774c5fe62e
blind675 <2725079+blind675@users.noreply.github.com>
2023-11-07 12:14:05 +0200

    update email

--- src/services/emailServices.ts
+++ src/services/emailServices.ts
@@ -5,8 +5,8 @@ export async function sendEmail() {
   const password = String(process.env.MAILTRAP_PASSWORD);
 
   const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: "live.smtp.mailtrap.io",
+    port: 587,
     auth: {
       user: username,
       pass: password,
@@ -19,18 +19,18 @@ export async function sendEmail() {
     from: '"Open360" <no-reply@open360.wiki>', // sender address
     to: recipients, // list of receivers
     subject: "Welcome to Open360", // Subject line
-    text: "\n \n \tWelcome, we are so happy you joined us! \n \n Nice day, \n \t The Open360 Team", // plain text body
+    text: "\n \n \tWe are so happy you joined us! \n \n Nice day, \n \t The Open360 Team", // plain text body
     html: `<div>
-            <b>Welcome to Open360</b>
+            <b>Welcome,</b>
             <br/>
             <br/>
-            <p>Welcome, we are so happy you joined us!</p>
+            <p>We are so happy you joined us!</p>
             <br/>
             <p>Nice day,</p>
             <br/>
             <div>
               <img src="https://www.open360.wiki/assets/logo.svg" alt="Open360 Logo" width="36" height="36"/>
-              <p style="font-weight: 700; color:black;" >Open360 <span style="color:rgb(29 101 149);">360</span>
+              <p style="font-weight: 700; color:black;" >Open <span style="color:rgb(29 101 149);">360</span>
               </p>
             </div>
           </div>`,
